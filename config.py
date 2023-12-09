2aeb0b6ba009dd6d6c505dbd08116b5080282a7f
Вячеслав <128319409+rodix1337@users.noreply.github.com>
2023-12-09 15:45:43 +0100

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-MySQL = ['localhost', 'user', 'password', 'support_db'] 
+MySQL = ['localhost', 'user', '1337', 'support_db'] 
 TOKEN = '6246926247:AAEo8UrhLqrw3lKNjjNPoYGcGSqVOAhISuE'
 ADMIN_ID = '6174050920'
 PROXY_URL = ''
